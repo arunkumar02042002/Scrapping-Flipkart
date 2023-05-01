b5a421c6286d1c94e808b7ab020662d0b01630e2
arunkumar02042002 <84587079+arunkumar02042002@users.noreply.github.com>
2023-05-01 13:49:54 +0530

    Added feature of stroing data into mysql table.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen
 import pymongo
 import csv
+import mysql.connector as connector
 
 # Taking input from the user
 search_key = input("Enter Product Name : ").replace(" ", "")
@@ -48,56 +49,110 @@ data_list = []
 
 # Storing all the review with name rating etc. in the form of dictionary in the data list
 for i in reviews_list:
-    #Name
+    # Name
     try:
         name = i.div.div.find("p", {"class": "_2sc7ZR _2V5EHH"}).text  # Name
     except:
-        name='Flipkart_user'
-    
-    #Rating
+        name = 'Flipkart_user'
+
+    # Rating
     try:
         rating = i.div.div.find("div", {"class": "_3LWZlK _1BLPMq"}).text
     except:
         rating = "0"
 
-    #Review
+    # Review
     try:
         review = i.div.div.find("p", {"class": "_2-N8zT"}).text
     except:
         review = "No Review"
-    
+
     # Comment
     try:
-        comment = i.div.div.find("div", {"class": ""}).text
+        comment = i.div.div.find("div", {"class": ""}).text[:500]
 
     except:
         comment = "No Comment"
-    
+
     data_list.append({
         "Name": name,
         "rating": rating,
-        "review": review,  
+        "review": review,
         "Comment": comment
     })
 
-# Storing the same data in respective csv file
-keys = data_list[0].keys()
-file_name = search_key + '.csv'
-with open(file_name, 'w', newline='', encoding='UTF-8') as output_file:
-    dict_writer = csv.DictWriter(output_file, keys)
-    dict_writer.writeheader()
-    dict_writer.writerows(data_list)
+# Providing options to the user
+print("Enter 1 to store data in csv file.")
+print("Enter 2 to store data in MongoDB.")
+print("Enter 3 to store data in MySQL.")
+choice = input("Enter your choice: ")
 
-# Storing all the data in the MongoDB
-# Connecting to the mongo
-client = pymongo.MongoClient(
-    "mongodb+srv://user1:user1@cluster0.zs8zanp.mongodb.net/?retryWrites=true&w=majority")
+try:
+    choice = int(choice)
+    match choice:
+        case 1:
+            # Storing the same data in respective csv file
+            keys = data_list[0].keys()
+            file_name = search_key + '.csv'
+            with open(file_name, 'w', newline='', encoding='UTF-8') as output_file:
+                dict_writer = csv.DictWriter(output_file, keys)
+                dict_writer.writeheader()
+                dict_writer.writerows(data_list)
 
-# Creating a databse named review_scrapped_data
-db = client["review_scrapped_data"]
+            print(f"Data inserted into {file_name}")
 
-# Creating respective collection for each search_key
-collection = db[search_key]
+        case 2:
+            # Storing all the data in the MongoDB
+            # Connecting to the mongo
+            client = pymongo.MongoClient(
+                "mongodb+srv://user1:user1@cluster0.zs8zanp.mongodb.net/?retryWrites=true&w=majority")
 
-# Inserting all the data at once using insert_many function
-collection.insert_many(data_list)
+            # Creating a databse named review_scrapped_data
+            db = client["review_scrapped_data"]
+
+            # Creating respective collection for each search_key
+            collection = db[search_key]
+
+            # Inserting all the data at once using insert_many function
+            collection.insert_many(data_list)
+
+            print(
+                f"Data inserted into MongoDB database = review_scrapped_data and collection = {search_key}")
+
+        case 3:
+            try:
+                # Connecting to the MySQL
+                db = connector.connect(host='localhost',
+                                    port='3306',
+                                    user='root',
+                                    password='mysqlofarun')
+                
+                # Creating a cursor
+                cur = db.cursor()
+
+                # Creating a database
+                query_db = "Create database if not exists review_scrapped_data"
+                cur.execute(query_db)
+
+                # Using the databse
+                query_db = "use review_scrapped_data"
+                cur.execute(query_db)
+
+                # Creating a table
+                query_table = "CREATE TABLE IF NOT EXISTS " + search_key + " (name varchar(30), rating varchar(1), review varchar(30), comment varchar(500))"
+                cur.execute(query_table)
+
+                # Storing data to the newly created table
+                for i in data_list:
+                    query = "INSERT INTO " + search_key + " VALUES (" + ", ".join("'" + str(value).replace("'", "''") + "'" for value in i.values()) + ")"
+                    cur.execute(query)
+
+                print(f"Data inserted into MySQL database = review_scrapped_data and table = {search_key}")
+
+            except Exception as e:
+                print(e)
+
+        case _:
+            print("Wrong input!")
+except:
+    print("An integer must be entered!")
